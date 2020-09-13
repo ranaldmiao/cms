fb6e7ee5301eafbe6056258fa4413012d9e2f0cf
Ranald Lam <ranaldmiao@gmail.com>
2020-09-13 15:13:31 +0800

    feat: Allow SVG images in Ranking

--- cmsranking/RankingWebServer.py
+++ cmsranking/RankingWebServer.py
@@ -389,7 +389,8 @@ class ImageHandler:
         'png': 'image/png',
         'jpg': 'image/jpeg',
         'gif': 'image/gif',
-        'bmp': 'image/bmp'
+        'bmp': 'image/bmp',
+        'svg': 'image/svg+xml'
     }
 
     MIME_TO_EXT = dict((v, k) for k, v in EXT_TO_MIME.items())
