82cf3209dd93f08ef32c3e4a321d8ea2621612a7
Ranald Lam <ranaldmiao@gmail.com>
2020-09-16 23:15:58 +0800

    feat: Show username in Team field

--- cmsranking/static/Scoreboard.js
+++ cmsranking/static/Scoreboard.js
@@ -240,10 +240,10 @@ var Scoreboard = new function () {
 
         if (user['team']) {
             result += " \
-    <td class=\"team\" title=\"" + DataStore.teams[user["team"]]["name"] + "\">" + user['team'] + "</td>";
+    <td class=\"team\" title=\"" + DataStore.teams[user["team"]]["name"] + "\">" + user['key'] + "</td>";
         } else {
             result += " \
-    <td class=\"team\"></td>";
+    <td class=\"team\">" + user['key'] + "</td>";
         }
 
         var contests = DataStore.contest_list;
@@ -405,11 +405,10 @@ var Scoreboard = new function () {
 
         $row.children("td.f_name").text(user["f_name"]);
         $row.children("td.l_name").text(user["l_name"]);
+        
+        $row.children(".team").text(user['key']);
         if (user["team"]) {
-            $row.children(".team").text(user['team']);
             $row.children(".team").attr("title", DataStore.teams[user["team"]]["name"]);
-        } else {
-            $row.children(".team").text("");
         }
     };
 
